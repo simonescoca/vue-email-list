ce1d4f9789f6da31184d2b8a5ce40159c8eb197f
simonescoca <simone.scoca@icloud.com>
2023-06-13 15:08:41 +0200

    added ul with email list from boolean api

--- script.js
+++ script.js
@@ -13,11 +13,20 @@ const {createApp} = Vue
 createApp ({
     data () {
         return{
-
+            emailList: []
         }
     },
 
-    methods: {
-        
+    created () {
+        for (let i = 0 ; i < 10 ; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(
+                (element) => {
+                    const email = element.data.response
+                    if (!this.emailList.includes(email)){
+                        this.emailList.push(email)
+                    }
+                }
+            )
+        }
     }
 }).mount ("#app")
