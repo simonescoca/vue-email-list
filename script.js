d660fd1bb3cda7d44a1278dc69224dcc763e0a98
simonescoca <simone.scoca@icloud.com>
2023-06-13 13:38:11 +0200

    file init

---
+++ script.js
@@ -0,0 +1,23 @@
+// # DESCRIZIONE
+    // Attraverso l'apposita API di Boolean:
+    // * https://flynn.boolean.careers/exercises/api/random/mail
+    // generare 10 indirizzi email e stamparli in pagina all'interno di una lista usando Vue e Axios come visto a lezione.
+
+// ? Bonus
+    // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+// $ NB
+    // Le mail posso anche essere uguali tra di loro, non è necessario verificare che siano diverse.
+
+const {createApp} = Vue
+createApp ({
+    data () {
+        return{
+
+        }
+    },
+
+    methods: {
+        
+    }
+}).mount ("#app")
